bcf145ae8a77bd5aec3291d7f301b38b1639faa4
toto <antoniolabate19@gmail.com>
2022-07-21 13:47:03 +0200

    plotting

--- python/battery_graphs/lib.py
+++ python/battery_graphs/lib.py
@@ -1,3 +1,20 @@
+# graphics
+def graph_info_field(d, field):
+    lists = sorted(d.items()) # sorted by key, return a list of tuples
+    
+    x, values = zip(*lists) # unpack a list of pairs into two tuples
+    
+    y = ()
+    for value in values:
+        y += int(value[field]),
+
+    plt.gcf().autofmt_xdate() # date autoformatting
+
+    plt.title(field)
+    plt.plot(x, y)
+    plt.show()
+
+
 # dictionary stuff
 def dict_prettify(dictionary):
     for x in dictionary:
