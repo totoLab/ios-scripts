0adf6c6bf06b5e88b3d2f793c9f99cb7cc35e3e4
Domenico <50798132+Domefemia@users.noreply.github.com>
2022-09-30 17:40:38 +0200

    Stessa roba fatta al codice precedente

--- python/battery_graphs/lib.py
+++ python/battery_graphs/lib.py
@@ -1,65 +1,69 @@
-from datetime import datetime
-from collections import OrderedDict
-import matplotlib.pylab as plt
 import re
 import sys
+from collections import OrderedDict
+from datetime import datetime
+
+import matplotlib.pylab as plt
 
 # configuration stuff
+
+
 def valid_config(config):
     minimum_config_options = ["storage", "fields"]
-    missing_options = []
-    for option in minimum_config_options:
-        if option not in config.keys():
-            missing_options.append(option)
-
-    if len(missing_options) > 0: 
-        raise RuntimeError(f"Configuration is missing some fields: {missing_options}")
+    if missing_options := [option for option in minimum_config_options if option not in config.keys()]:
+        raise RuntimeError(
+            f"Configuration is missing some fields: {missing_options}")
     return True
+
 
 def extract_config(config_file):
     ret = {}
     with open(config_file, "r") as f:
         current_config = None
-        for line in f.readlines():
+        for line in f:
             line = line.strip()
-            
+
             match = re.search("\[[a-zA-Z]+\]", line)
             if match is not None:
                 start, end = match.span()
-                current_config = line[start+1:end-1].strip()
+                current_config = line[start + 1:end - 1].strip()
             else:
-                if current_config is not None:
-                    if current_config not in ret:
-                        ret[current_config] = {}
-                    if "=" in line:
-                        option, value = tuple([x.strip() for x in line.split("=")])
-                        ret[current_config].update({option: value}) 
-                else:
+                if current_config is None:
                     raise RuntimeError("Configuration file is not valid.")
+                if current_config not in ret:
+                    ret[current_config] = {}
+                if "=" in line:
+                    option, value = tuple(x.strip() for x in line.split("="))
+                    ret[current_config].update({option: value})
 
-    if valid_config(ret):   
+    if valid_config(ret):
         return ret
+
 
 def check_chosen_device(available_devices):
     args = sys.argv
     if len(args) < 2:
         if len(available_devices) > 1:
-            raise RuntimeError("No option was given as argument to the command. Retry.")
+            raise RuntimeError(
+                "No option was given as argument to the command. Retry.")
         else:
             choice = list(available_devices.keys())[0]
     else:
         choice = args[1].strip()
         if choice not in available_devices:
-            raise RuntimeError(f"{choice} is not an available device, if you have the logs, specify in the config file how to reach them.")
+            raise RuntimeError(
+                f"{choice} is not an available device, if you have the logs, specify in the config file how to reach them.")
 
     return choice
 
 # graphics
+
+
 def graph_info_field(d, field):
-    lists = sorted(d.items()) # sorted by key, return a list of tuples
-    
-    keys, values = zip(*lists) # unpack a list of pairs into two tuples
-    
+    lists = sorted(d.items())  # sorted by key, return a list of tuples
+
+    keys, values = zip(*lists)  # unpack a list of pairs into two tuples
+
     x = ()
     for key in keys:
         vertical_key = ""
@@ -85,17 +89,18 @@ def dict_prettify(dictionary):
     for x in dictionary:
         print(x)
         for y in dictionary[x]:
-            print(y + ': ' + dictionary[x][y])
+            print(f'{y}: {dictionary[x][y]}')
+
 
 def order_dict(d):
     return OrderedDict(
-    sorted(d.items(), key = lambda x:datetime.strptime(x[0], '%Y-%m-%d')) )
+        sorted(d.items(), key=lambda x: datetime.strptime(x[0], '%Y-%m-%d')))
+
 
 def regex_span(regex_expr, string):
     match = re.search(regex_expr, string)
     if match is None:
         return None
-    else:
-        match = match.span()
+    match = match.span()
     start, end = match[0], match[1]
     return string[start:end]
